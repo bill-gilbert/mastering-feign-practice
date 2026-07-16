0013cf2b253909a0132b831e077895c94abfc085
Pavel Glukhov <pvglukhov@gmail.com>
2026-07-16 11:51:22 +0300

    [spring feign] добавлено обновление конфига

--- client-retry-circuitbreaker/src/main/java/com/example/caller/client/CalleeClient.java
+++ client-retry-circuitbreaker/src/main/java/com/example/caller/client/CalleeClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "calleeService", url = "${callee.url:http://localhost:8080}", fallbackFactory = CalleeFallbackFactory.class)
+@FeignClient(name = "calleeService", url = "${callee.url:http://localhost:18080}", fallbackFactory = CalleeFallbackFactory.class)
 public interface CalleeClient {
     @GetMapping("/api/response")
     ResponseData getResponseWithMode(@RequestParam String mode);
--- client-retry-circuitbreaker/src/main/java/com/example/caller/controller/CallController.java
+++ client-retry-circuitbreaker/src/main/java/com/example/caller/controller/CallController.java
@@ -2,21 +2,19 @@ package com.example.caller.controller;
 
 import com.example.caller.client.CalleeClient;
 import com.example.caller.controller.dto.ResponseData;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/call")
+@RequiredArgsConstructor
 public class CallController {
     private final CalleeClient calleeClient;
 
-    public CallController(CalleeClient calleeClient) {
-        this.calleeClient = calleeClient;
-    }
-
     @GetMapping
     public ResponseData call() {
-        return calleeClient.getResponseWithMode();
+        return calleeClient.getResponseWithMode("mode");
     }
 }
